9f767fea05f0a8d02443607d17d29e8a5d2d033c
Sapa.Tech <sapar.academy@gmail.com>
2020-03-08 17:12:54 +0600

    Initial Commit

---
+++ ViewGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewGenerator
+//
+//  Created by User on 3/8/20.
+//  Copyright © 2020 sapa.tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
